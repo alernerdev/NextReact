0db40f8cee584350000ceb839c57a91114d91f43
Alex Lerner <alernerdev@gmail.com>
2018-10-24 14:01:38 -0400

    added batman page with async data fetch

--- pages/batman.js
+++ pages/batman.js
@@ -1,37 +1,35 @@
 /* eslint-disable react/react-in-jsx-scope */
-
 import Link from 'next/link';
-import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout';
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/prop-types */
-const PostLink = props => (
-  <li>
-    <Link href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
+const Batman = (props) => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(({ show }) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 );
-/* eslint-enable jsx-a11y/anchor-is-valid */
 
-/* eslint-disable jsx-a11y/anchor-is-valid */
-// the as property hides the query string business on the browser URL
-const Batman = (props) => {
-  return (
-    <Layout>
-      <h1>Batman TV Shows</h1>
-      <ul>
-        {props.shows.map(({ show }) => (
-          <li key={show.id}>
-            <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Layout>
-  );
+Batman.getInitialProps = async function getInitialProps() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  /* eslint-disable no-console */
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data
+  };
 };
 
 export default Batman;
